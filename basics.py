dfabb10912d727bde268e6a7185c5f3ba9019eee
10orabh <123290989+10orabh@users.noreply.github.com>
2026-02-10 07:31:32 +0530

    add show method to show the detail of the employee

--- basics.py
+++ basics.py
@@ -4,9 +4,12 @@ class Employee:
         self.salary = salary
         self.designation = designation
 
-   
-
+    def show(self):
+        print(f"id = {self.id}")
+        print(f"salary = {self.salary}")
+        print(f"designation = {self.designation}")
+        
     
 emp1 = Employee(24360,20000,"Assitant_professor")
-
+emp1.show()
 
